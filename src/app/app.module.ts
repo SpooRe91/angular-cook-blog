605cb2aa0f777f3d72a54dc9a6a841b10cead3a3
Martin B <m.bogdanov9110@gmail.com>
2022-11-07 19:33:20 +0200

    update on app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
