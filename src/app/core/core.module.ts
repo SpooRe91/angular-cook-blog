b2bd89a805baa18e62a88e4395b9955f2d5b27a8
Martin B <m.bogdanov9110@gmail.com>
2022-11-20 19:11:58 +0200

    added new Components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,29 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { MacrosComponent } from './macros/macros.component';
 import { SharedModule } from '../shared/shared.module';
+import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 
 @NgModule({
   declarations: [
     NavigationComponent,
     FooterComponent,
     MacrosComponent,
+    GlobalLoaderComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
+    RouterModule,
   ],
   exports: [
     NavigationComponent,
     FooterComponent,
-    MacrosComponent
+    MacrosComponent,
+    GlobalLoaderComponent
   ]
 })
 export class CoreModule { }
