767be1b50b3b7d167fce639f49b44fbcaef093a4
Martin B <m.bogdanov9110@gmail.com>
2022-11-20 19:11:46 +0200

    created API endpoints

--- src/app/API/endpoints.ts
+++ src/app/API/endpoints.ts
@@ -1,21 +1,21 @@
-// export const API_URL = "http://localhost:3030";//GET
+export const API_URL = "http://localhost:3030";//GET
 
 export const endpoints = {
-    //REGULAR + MODIFICATIONS***************
-    API_DETAILS: (id: string | number) => `/details/${id}`,//GET req
-    API_EDIT: (id: string | number) => `/edit/${id}`,//PUT req
-    API_DELETE: (id: string | number) => `/delete/${id}`,//DELETE req
-    API_LIKE: (id: string | number) => `/like/${id}`,//GET req
-    API_GET_USER: (id: string | number) => `/user-get/${id}`,//GET req
-    API_EDIT_USER_IMAGE: (id: string | number) => `/user-edit/${id}`,//PUT req
-    //AUTH**********************************
-    API_REGISTER: `/auth/register`,//POST req
-    API_LOGIN: `/auth/login`,//POST req
-    API_LOGOUT: `/auth/logout`,//GET req
-    //RECIPE********************************
-    API_BROWSE: `/recipe/browse`,//GET req
-    API_MYRECIPES: `/recipe/myRecipes`,//GET req
-    API_MACROS: `/recipe/macros`,//GET req
-    API_ADD: `/recipe/add`,//POST req
-    // *************************************
+  //REGULAR + MODIFICATIONS***************
+  API_DETAILS: (id: string | number) => `/details/${id}`,//GET req
+  API_EDIT: (id: string | number) => `/edit/${id}`,//PUT req
+  API_DELETE: (id: string | number) => `/delete/${id}`,//DELETE req
+  API_LIKE: (id: string | number) => `/like/${id}`,//GET req
+  API_GET_USER: (id: string | number) => `/user-get/${id}`,//GET req
+  API_EDIT_USER_IMAGE: (id: string | number) => `/user-edit/${id}`,//PUT req
+  //AUTH**********************************
+  API_REGISTER: `/auth/register`,//POST req
+  API_LOGIN: `/auth/login`,//POST req
+  API_LOGOUT: `/auth/logout`,//GET req
+  //RECIPE********************************
+  API_BROWSE: `/recipe/browse`,//GET req
+  API_MYRECIPES: `/recipe/myRecipes`,//GET req
+  API_MACROS: `/recipe/macros`,//GET req
+  API_ADD: `/recipe/add`,//POST req
+  // *************************************
 }
