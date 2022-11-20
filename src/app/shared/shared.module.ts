09cb4721f5a2362c89810177fa938317f08e879e
Martin B <m.bogdanov9110@gmail.com>
2022-11-20 19:16:01 +0200

    update on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 
-
-
 @NgModule({
   declarations: [
-    LoaderComponent
+    LoaderComponent,
   ],
   imports: [
     CommonModule
