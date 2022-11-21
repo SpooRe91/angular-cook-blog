f25c90763757595cae93017bfc75af67cdcfb102
Martin B <m.bogdanov9110@gmail.com>
2022-11-21 22:17:37 +0200

    added router to recipe module

--- src/app/recipes/recipe.module.ts
+++ src/app/recipes/recipe.module.ts
@@ -1,14 +1,11 @@
+import { ListComponent } from './browse/list.component';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from './../core/core.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ListComponent } from './list/list.component';
 import { SharedModule } from '../shared/shared.module';
 import { RecipeDetails } from './details/details.component';
-import { MacrosComponent } from '../core/macros/macros.component';
-import { AuthGuard } from '../Guards/user-guard';
-
 
 @NgModule({
   declarations: [
@@ -19,16 +16,7 @@ import { AuthGuard } from '../Guards/user-guard';
     CommonModule,
     CoreModule,
     SharedModule,
-    RouterModule.forChild([
-      {
-        path: 'recipe/macros',
-        component: MacrosComponent
-      },
-      {
-        path: 'recipe/details/:id',
-        component: RecipeDetails
-      },
-    ])
+    RouterModule
   ],
   exports: [
     ListComponent
