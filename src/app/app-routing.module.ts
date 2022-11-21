3a306252dc0f75e27a892980c1d33e20a4648663
Martin B <m.bogdanov9110@gmail.com>
2022-11-21 22:15:08 +0200

    removed recipe routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 // import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ListComponent } from './recipes/list/list.component';
 import { HomepageComponent } from './homepage/homepage.component';
+
 
 const routes: Routes = [
   {
@@ -13,27 +13,6 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomepageComponent
-  },
-
-  {
-    path: 'recipe/browse',
-    pathMatch: 'full',
-    redirectTo: '/recipe/browse'
-  },
-  {
-    path: 'recipe/browse',
-    component: ListComponent
-  },
-
-  {
-    path: 'recipe/macros',
-    pathMatch: 'full',
-    redirectTo: '/recipe/macros'
-  },
-  {
-    path: 'recipe/details/:id',
-    pathMatch: 'full',
-    redirectTo: '/recipe/details/:id'
   }
 
 ];
