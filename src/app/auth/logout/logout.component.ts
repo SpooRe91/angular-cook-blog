999fa98d5a91be95178b737465882bb985ade85c
Martin B <m.bogdanov9110@gmail.com>
2022-11-24 00:03:54 +0200

    created logout comp+ routing for it and some basing logout logic

---
+++ src/app/auth/logout/logout.component.ts
@@ -0,0 +1,19 @@
+import { IUser } from './../../interfaces/user';
+import { AuthService } from './../auth.service';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss']
+})
+export class LogoutComponent {
+
+  constructor(private router: Router, private authService: AuthService) {
+
+    this.authService.user = null;
+    this.router.navigate(['home']);
+  }
+
+}
