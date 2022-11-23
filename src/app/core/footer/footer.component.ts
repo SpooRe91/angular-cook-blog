7dace7e311b0678792f11ef88f08f39591567541
Martin B <m.bogdanov9110@gmail.com>
2022-11-24 00:09:11 +0200

    updated footer with conditional rendering options

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
   }
+
+  constructor(private authService: AuthService) { }
 
 }
