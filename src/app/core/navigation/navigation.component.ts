dae9815e35747400ed2b0ef8d3ce2e75daa1b9c2
Martin B <m.bogdanov9110@gmail.com>
2022-11-30 22:29:09 +0200

    created and updated navigation

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,7 +1,8 @@
-import { AuthService } from './../../auth/auth.service';
+import { IUser } from './../../interfaces/user';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
+import { AuthService } from './../../auth/auth.service';
 import { GlobalLoaderService } from './../services/global-loader.service';
 @Component({
   selector: 'app-navigation',
@@ -13,20 +14,18 @@ export class NavigationComponent {
   path: string = "../../assets/logo.ico";
   alt: string = '#';
 
-  get isLoggedIn() {
-    return this.authService.isLoggedIn;
+  get isLogged(): boolean | null {
+    return this.authService.isLogged;
   }
 
-  get user() {
+  get getUser() {
     return this.authService.user;
   }
 
-
   constructor(
     public globalLoaderService: GlobalLoaderService,
+    private authService: AuthService,
     private router: Router,
-    private authService: AuthService
   ) { }
-
 
 }
