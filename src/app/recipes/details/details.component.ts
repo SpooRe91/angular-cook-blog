e46097fefa291c09e26f5f07d8518a6390a6dd9d
Martin B <m.bogdanov9110@gmail.com>
2022-11-30 22:33:19 +0200

    created and updated details

--- src/app/recipes/details/details.component.ts
+++ src/app/recipes/details/details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { GlobalLoaderService } from './../../core/services/global-loader.service';
 import { IRecipe } from './../../interfaces/recipeInterface';
 import { RecipeService } from '../recipe.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detials',
@@ -14,6 +15,8 @@ export class RecipeDetails implements OnInit {
   alt: string = "#";
   recipeDetails: IRecipe | null = null;
   params: string | number;
+  date: string | Date = '';
+  isOwner = true;
 
   constructor(
     private recipeService: RecipeService,
@@ -30,9 +33,18 @@ export class RecipeDetails implements OnInit {
 
       next: (value) => {
         if (value === null && value === undefined) { return }
+        // TODO:
+        // if (value.owner === user._id)
+        // { this.isOwner = true }
 
         this.recipeDetails = value;
         this.globalLoaderService.hideLoader();
+        this.date = new Date(value.createdAt.toString());
+        this.date = this.date.toString()
+          .slice(0, this.date
+            .toString()
+            .indexOf('GMT'));
+        console.log(typeof this.date);
         console.log(this.recipeDetails);
         return
       },
