1a99b6bd91968f4ba0333567c651a70c08d9b400
Martin B <m.bogdanov9110@gmail.com>
2022-11-30 22:35:29 +0200

    added login and logout to auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,53 @@
+import { Router } from '@angular/router';
+// import { IUser, IUserLogin } from './../interfaces/user';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+// import { IUser } from '../interfaces';
+import { API_URL, endpoints } from '../API/endpoints';
 import { IUser } from '../interfaces';
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  user: IUser | null = null;
+  user!: IUser | null;
+  isLogged: boolean | null = null;
 
-  get isLoggedIn() {
-    return this.user !== null;
+  setUserStatus(status: boolean) {
+    return this.isLogged = status;
   }
 
-  constructor() { }
+  setUser(user: IUser | null) {
+    return this.user = user;
+  }
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) { }
+
+  userLogin<IUserLogin>(loginData: IUserLogin) {
+    return this.http.post(API_URL + endpoints.API_LOGIN, loginData, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+        'Allow-Control-Access-Policy': "true",
+        'Access-Control-Allow-Credentials': "true",
+      }
+    }).subscribe({
+      next: (value) => {
+        if (!value.hasOwnProperty('email')) { return };
+        console.log('logged in');
+        this.router.navigate(['home']);
+        return (this.setUser(value), this.setUserStatus(true));
+      },
+      error: (err) => {
+        console.log(err.error.message);
+        return;
+      }
+    })
+  }
 
 }
-
