05fc98de69e75315340ccacb9704f0e3098b2c46
Martin B <m.bogdanov9110@gmail.com>
2022-12-01 23:24:48 +0200

    tests for min length

---
+++ src/app/shared/min-length.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MinLengthDirective } from './min-length.directive';
+
+describe('MinLengthDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MinLengthDirective();
+    expect(directive).toBeTruthy();
+  });
+});
