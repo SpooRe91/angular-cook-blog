55094138e06d7250075c0d21115965c8381d1efc
Martin B <m.bogdanov9110@gmail.com>
2022-12-01 23:23:54 +0200

    updated register form and register method, auto login + validators and messages

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IUser } from './../../interfaces/user';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  user: IUser | null = null;
+  isLogged: boolean | null = null;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  handleOnSubmitForm(form: NgForm) {
+    if (form.invalid) { return; }
+    const value: { email: string, password: string, rePassword: string } = form.value;
+    console.log(value);
+    //TODO: provide an error somewhere in the form
+    this.authService.userRegister(value);
   }
 
 }
