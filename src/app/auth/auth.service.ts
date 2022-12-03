a6e52a84015157caec5c6bfc495873e16de5088f
Martin B <m.bogdanov9110@gmail.com>
2022-12-03 17:35:06 +0200

    updated userLogout, and login created check if logged

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { GlobalLoaderService } from './../core/services/global-loader.service';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { IUser, IUserAuth } from './../interfaces/user';
 import { API_URL, endpoints } from '../API/endpoints';
+import { GlobalLoaderService } from './../core/services/global-loader.service';
+import { setSession, getSession, logoutSession } from '../API/session';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,7 @@ export class AuthService {
 
   user!: IUser | null;
   isLogged: boolean | null = null;
+  hasError: null | string = null;
 
   setUserStatus(status: boolean) {
     return this.isLogged = status;
@@ -29,8 +31,22 @@ export class AuthService {
     private globalLoaderService: GlobalLoaderService,
   ) { }
 
+  handleClearError() {
+    return this.hasError = null;
+  }
+
+  checkIfLogged() {
+    if (getSession()) {
+      this.globalLoaderService.showLoader("Loading");
+      this.hasError = "You are already logged in!";
+      this.router.navigate(['home']);
+      this.globalLoaderService.hideLoader();
+    };
+    return;
+  }
+
   userLogin(loginData: IUserAuth) {
-    this.http.post<IUserAuth>(API_URL + endpoints.API_LOGIN, loginData, {
+    return this.http.post<IUserAuth>(API_URL + endpoints.API_LOGIN, loginData, {
       withCredentials: true,
       headers: {
         'Content-Type': 'application/json',
@@ -43,15 +59,16 @@ export class AuthService {
         if (!user.email) { return };
         console.log('logged in as - ', user);
         this.router.navigate(['home']);
-        return (
-          this.setUser(user),
-          this.setUserStatus(true),
-          this.globalLoaderService.hideLoader()
-        )
+
+        setSession(user);
+        this.setUser(user);
+        this.setUserStatus(true);
+        this.globalLoaderService.hideLoader();
       },
       error: (err) => {
         console.log(err.error.message);
-        return err.error.message;
+        console.error(err.error.message);
+        return this.hasError = err.error.message;
       }
     })
   }
@@ -73,13 +90,38 @@ export class AuthService {
         this.setUser(res);
         this.globalLoaderService.hideLoader();
         this.router.navigate(['home']);
-        return res;
+        return this.user;
       },
       error: (err) => {
         console.log(err.error.message);
-        return err.error.message;
+        return this.hasError = err.error.message;
+      }
+    });
+  }
+
+  userLogout() {
+    return this.http.get(API_URL + endpoints.API_LOGOUT, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+        'Allow-Control-Access-Policy': "true",
+        'Access-Control-Allow-Credentials': "true",
+      }
+    }).subscribe({
+      next: () => {
+        this.globalLoaderService.showLoader('Logging out');
+        console.log('Logged out!');
+        this.user = null;
+        this.isLogged = false;
+        logoutSession();
+        this.globalLoaderService.hideLoader();
+        this.router.navigate(['auth/login']);
+      },
+      error: (err) => {
+        console.log(err.error.message);
+        this.router.navigate(['auth/login']);
+        return this.hasError = err.error.message;
       }
     })
   }
-
 }
