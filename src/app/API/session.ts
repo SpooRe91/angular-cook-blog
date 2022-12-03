5746a76ceeb44b72779f1261a30d6a620084a8c5
Martin B <m.bogdanov9110@gmail.com>
2022-12-03 17:25:47 +0200

    created sessions file with local and session storage methods

---
+++ src/app/API/session.ts
@@ -0,0 +1,21 @@
+import { IUser } from './../interfaces/user';
+export const setSession = ({ email, id, image, token }: IUser) => {
+
+  const user = { email, id, image, token };
+  sessionStorage.setItem("User", JSON.stringify(user));
+  localStorage.setItem("User", JSON.stringify(user));
+}
+
+export const getSession = () => {
+  const session = sessionStorage.getItem('User')
+  const local = localStorage.getItem('User');
+  return (
+    session ? JSON.parse(session) : null,
+    local ? JSON.parse(local) : null
+  );
+}
+
+export const logoutSession = () => {
+  sessionStorage.removeItem('User');
+  localStorage.removeItem('User');
+}
