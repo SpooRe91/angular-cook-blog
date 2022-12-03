8daf9537bd32e111c621fbc21368daa16e5c0613
Martin B <m.bogdanov9110@gmail.com>
2022-12-03 17:25:24 +0200

    created a check in the app comp for  logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { GlobalLoaderComponent } from './core/global-loader/global-loader.component';
+import { AuthService } from './auth/auth.service';
 import { Component } from '@angular/core';
+import { getSession } from './API/session';
+import { GlobalLoaderService } from './core/services/global-loader.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cook-blog';
+
+  constructor(
+    private authService: AuthService,
+    private globalLoaderService: GlobalLoaderService
+  ) {
+    globalLoaderService.showLoader("Loading");
+    if (!getSession()) { return }
+    this.globalLoaderService.hideLoader();
+    return (
+      this.authService.isLogged = true,
+      this.authService.user = getSession()
+    );
+  }
 }
