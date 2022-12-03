ba2220f7a3a67014e08633598d5685e768804e90
Martin B <m.bogdanov9110@gmail.com>
2022-12-04 01:47:11 +0200

    update on structure of component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,9 @@
+import { getSession } from 'src/app/API/session';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 import { faAt, faKey } from '@fortawesome/free-solid-svg-icons';
 
 import { IUser } from './../../interfaces/user';
-import { getSession } from 'src/app/API/session';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -15,19 +15,32 @@ export class LoginComponent {
   faEmail = faAt;
   faPass = faKey;
 
-  user: IUser | null = null;
-  isLogged: boolean | null = null;
-
   constructor(
     public authService: AuthService,
   ) {
-    this.authService.checkIfLogged();
+    //if not logged and redirected here
+    if (!getSession() && this.authService.isRedirected) {
+      this.authService.hasError = "You need to login first!";
+    } else if (!getSession() && !this.authService.isRedirected) {
+      this.authService.hasError = null;
+    } else {
+      this.authService.checkIfLogged("You are already logged in!");
+    }
   }
 
   handleOnSubmitForm(form: NgForm) {
     if (form.invalid) { return; };
+    this.authService.hasError = null;
     const value: { email: string, password: string } = form.value;
     console.log(value);
     this.authService.userLogin(value);
+  }
+
+  ngOnDestroy(): void {
+    if (this.authService.hasError && this.authService.isRedirected) {
+      this.authService.hasError = null;
+      this.authService.isRedirected = false;
+    }
+    return;
   }
 }
