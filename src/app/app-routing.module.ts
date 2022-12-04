e32a4334a30502d3bda853250d3c43296dddefa6
Martin B <m.bogdanov9110@gmail.com>
2022-12-04 13:18:38 +0200

    added page not found comp to app-router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 // import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
 import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     pathMatch: 'full',
-    redirectTo: '/home'
+    component: HomepageComponent
   },
   {
-    path: 'home',
-    component: HomepageComponent
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
   }
 ];
 export const AppRoutingModule = RouterModule.forRoot(routes);
