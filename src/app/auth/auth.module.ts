46b34068d998702171ee0c65b89cf65f1dea413b
Martin B <m.bogdanov9110@gmail.com>
2022-12-06 16:30:55 +0200

    update on auth module and auth service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
-import { RegisterComponent } from './register/register.component'; import { MinLengthDirective } from '../shared/min-length.directive';
-import { SharedModule } from '../shared/shared.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -18,7 +18,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     LoginComponent,
     RegisterComponent,
     LogoutComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     AuthRoutingModule,
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,8 +4,9 @@ import { HttpClient } from '@angular/common/http';
 
 import { IUser, IUserAuth } from './../interfaces/user';
 import { API_URL, endpoints } from '../API/endpoints';
-import { GlobalLoaderService } from './../core/services/global-loader.service';
+import { GlobalLoaderService } from '../shared/services/global-loader.service';
 import { setSession, getSession, logoutSession } from '../API/session';
+import { headers } from '../constants/requestHeaders';
 
 @Injectable({
   providedIn: 'root'
@@ -29,14 +30,13 @@ export class AuthService {
   }
 
   checkIfLogged(message?: string | null) {
-    this.globalLoaderService.showLoader("Loading");
-
+    this.globalLoaderService.showLoader("Loading", true);
     if (getSession()) {
       this.hasError = message || "";
       this.router.navigate(['home']);
-      this.globalLoaderService.hideLoader();
       return;
     };
+    this.globalLoaderService.hideLoader(false);
     this.hasError = message || "";
     this.router.navigate(['auth/login']);
     this.isRedirected = true;
@@ -51,15 +51,8 @@ export class AuthService {
   }
 
   userLogin(loginData: IUserAuth) {
-    this.globalLoaderService.showLoader("Loading");
-    return this.http.post<IUserAuth>(API_URL + endpoints.API_LOGIN, loginData, {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'application/json',
-        'Allow-Control-Access-Policy': "true",
-        'Access-Control-Allow-Credentials': "true",
-      }
-    }).subscribe({
+    this.globalLoaderService.showLoader("Loading", true);
+    return this.http.post<IUserAuth>(API_URL + endpoints.API_LOGIN, loginData, headers).subscribe({
       next: (user) => {
         if (!user.email) { return };
         console.log('logged in as - ', user);
@@ -67,64 +60,65 @@ export class AuthService {
         setSession(user);
         this.setLoginInfo(user, true);
         this.router.navigate(['home']);
-        this.globalLoaderService.hideLoader();
+        this.globalLoaderService.hideLoader(false);
       },
       error: (err) => {
-        console.log(err.error.message);
+        if (!err.ok) {
+          console.error(err.message);
+          this.globalLoaderService.hideLoader(false);
+          return this.hasError = 'There is no connection to the server right now!';
+        }
         console.error(err.error.message);
-        return this.hasError = err.error.message;
+        this.globalLoaderService.hideLoader(false);
+        return this.hasError = err.error.messsage;
       }
     })
   }
 
   userRegister(registerData: IUserAuth) {
-    this.globalLoaderService.showLoader('Loading');
-    return this.http.post<IUserAuth>(API_URL + endpoints.API_REGISTER, registerData, {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'application/json',
-        'Allow-Control-Access-Policy': "true",
-        'Access-Control-Allow-Credentials': "true",
-      }
-    }).subscribe({
+    this.globalLoaderService.showLoader('Loading', true);
+    return this.http.post<IUserAuth>(API_URL + endpoints.API_REGISTER, registerData, headers).subscribe({
       next: (res) => {
         if (!res.email) { return };
         console.log('registered successfully:', res);
 
         this.setLoginInfo(res, true);
         this.router.navigate(['home']);
-        this.globalLoaderService.hideLoader();
         return this.user;
       },
       error: (err) => {
-        console.log(err.error.message);
-        return this.hasError = err.error.message;
+        if (!err.ok) {
+          console.error(err.message);
+          this.globalLoaderService.hideLoader(false);
+          return this.hasError = 'There is no connection to the server right now!';
+        }
+        this.globalLoaderService.hideLoader(false);
+        console.error(err.error.message);
+        return this.hasError = err.error.messsage;
       }
     });
   }
 
   userLogout() {
-    this.globalLoaderService.showLoader('Loading');
     if (!getSession()) { this.router.navigate(['auth/login']); return };
+    this.globalLoaderService.showLoader('Loading', true);
 
-    return this.http.get(API_URL + endpoints.API_LOGOUT, {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'application/json',
-        'Allow-Control-Access-Policy': "true",
-        'Access-Control-Allow-Credentials': "true",
-      }
-    }).subscribe({
+    return this.http.get(API_URL + endpoints.API_LOGOUT, headers).subscribe({
       next: () => {
         console.log('Logged out!');
         logoutSession();
         this.setLoginInfo(null, false);
         this.hasError = null;
         this.router.navigate(['auth/login']);
-        this.globalLoaderService.hideLoader();
       },
       error: (err) => {
-        console.log(err.error.message);
+        if (!err.ok) {
+          console.error(err.message);
+          this.globalLoaderService.hideLoader(false);
+          return this.hasError = 'There is no connection to the server right now!';
+        }
+        console.error(err.error.message);
+        this.globalLoaderService.hideLoader(false);
         this.router.navigate(['auth/login']);
         return this.hasError = err.error.message;
       }
