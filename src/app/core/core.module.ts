558ce2aaeeef9cffb78673f71927dcd1764a89c6
Martin B <m.bogdanov9110@gmail.com>
2022-12-06 16:33:01 +0200

    updated core modukle

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,7 +6,6 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { MacrosComponent } from './macros/macros.component';
 import { SharedModule } from '../shared/shared.module';
-import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
@@ -14,20 +13,18 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     NavigationComponent,
     FooterComponent,
     MacrosComponent,
-    GlobalLoaderComponent,
     PageNotFoundComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
-    AuthModule
+    AuthModule,
   ],
   exports: [
     NavigationComponent,
     FooterComponent,
     MacrosComponent,
-    GlobalLoaderComponent
   ]
 })
 export class CoreModule { }
