8ee49bde176b1f6ec676b979ace272c2e2c429ca
Martin B <m.bogdanov9110@gmail.com>
2022-12-07 15:06:06 +0200

    created user guard, still TODO

--- src/app/Guards/user-guard.ts
+++ src/app/Guards/user-guard.ts
@@ -1,23 +1,29 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 
+import { AuthService } from './../auth/auth.service';
+
+
 @Injectable({
   providedIn: 'root'
 })
 //TODO: MAKE THIS WORK FOR NON-LOGGED OR LOGGED USER!
-export class AuthGuard implements CanActivate {
+export class UserGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    //TODO:MAKE IT REDIRECT TO AN ERROR PAGE!
-    return this.checkIfLogged(state.url) || this.router.createUrlTree(["/home"]);
+    return this.checkIfLogged() || this.router.createUrlTree(["/"]);
   }
 
-  //THIS IS JUST AN EXAMPLE, TODO: ADEQUITE GUARD
-  checkIfLogged(url: string): boolean {
-    return false;
+  checkIfLogged(): boolean {
+    if (!this.authService.isLogged) {
+      this.authService.hasError = "You must login first!";
+    }
+    return this.authService.isLogged;
   }
-
 }
 
