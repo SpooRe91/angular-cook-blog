e4d073ba0ec1c912bad5f356ca53e92138e91df2
Martin B <m.bogdanov9110@gmail.com>
2022-12-07 15:39:29 +0200

    created create form + logic and routing

--- src/app/recipes/create/create.component.ts
+++ src/app/recipes/create/create.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { RecipeService } from './../recipe.service';
+import { AuthService } from './../../auth/auth.service';
+import {
+  Component,
+  OnDestroy
+} from '@angular/core';
+import { GlobalLoaderService } from 'src/app/shared/services/global-loader.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnDestroy {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    private recipeService: RecipeService,
+    public globalLoaderService: GlobalLoaderService
+  ) { }
 
-  ngOnInit(): void {
+  handleOnSubmitForm(form: NgForm) {
+    if (form.invalid) { return; };
+    this.globalLoaderService.showLoader("Loading", true);
+    this.authService.hasError = null;
+
+    const value: {
+      name: string,
+      image: string,
+      portions: number,
+      difficulty: string,
+      ingredients: string,
+      fullRecipe: string
+    } = form.value;
+
+    console.log(value);
+    this.recipeService.createRecipe(value);
+  }
+
+  ngOnDestroy(): void {
+    if (this.authService.hasError && this.authService.isRedirected) {
+      this.authService.hasError = null;
+      this.authService.isRedirected = false;
+      this.globalLoaderService.hideLoader(false);
+    }
+    return;
   }
 
 }
