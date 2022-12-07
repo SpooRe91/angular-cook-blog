2a903faff598eddcdf0bc4a4d049fb3845ec2ee7
Martin B <m.bogdanov9110@gmail.com>
2022-12-07 15:39:11 +0200

    created ICreateRecipe interface

--- src/app/interfaces/recipeInterface.ts
+++ src/app/interfaces/recipeInterface.ts
@@ -1,38 +1,51 @@
 export interface IRecipe {
-    createdAt: {
-        type: String,
-    },
-    difficulty: {
-        type: String,
-    },
-    fullRecipe: {
-        type: String,
-    },
-    image: {
-        type: String,
-    },
-    ingredients: {
-        type: String,
-    },
-    name: {
-        type: String,
-    },
-    owner: {
-        type: String,
-    },
-    ownerName: {
-        type: String,
-    },
-    portions: {
-        type: Number,
-    },
-    updatedAt: {
-        type: String,
-    },
-    __v: {
-        type: Number,
-    }
-    _id: {
-        type: String,
-    }
+  createdAt: {
+    type: String,
+  },
+  difficulty: {
+    type: String,
+  },
+  fullRecipe: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  ingredients: {
+    type: String,
+  },
+  isDeleted: {
+    type: Boolean,
+  }
+  name: {
+    type: String,
+  },
+  owner: {
+    type: String,
+  },
+  ownerName: {
+    type: String,
+  },
+  portions: {
+    type: Number,
+  },
+  updatedAt: {
+    type: String,
+  },
+  __v: {
+    type: Number,
+  }
+  _id: {
+    type: String,
+  }
 }
+
+export interface ICreateRecipe {
+  name: string,
+  image: string,
+  portions: number,
+  difficulty: string,
+  ingredients: string,
+  fullRecipe: string
+}
+
