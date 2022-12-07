7d35348fc9a5821c6eb235eb5d2cd9cd5a63478b
Martin B <m.bogdanov9110@gmail.com>
2022-12-07 15:39:49 +0200

    added basic sorting of the entire list + rendering

--- src/app/recipes/browse/list.component.ts
+++ src/app/recipes/browse/list.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 import { IRecipe } from './../../interfaces/recipeInterface';
@@ -13,24 +14,44 @@ import { GlobalLoaderService } from 'src/app/shared/services/global-loader.servi
 export class ListComponent implements OnInit {
   alt = "#";
   recipeList: IRecipe[] | null = null;
+  sortingType = 'newest';
+
 
   constructor(
-    private recipeService: RecipeService,
-    private globalLoaderService: GlobalLoaderService
+    public recipeService: RecipeService,
+    private globalLoaderService: GlobalLoaderService,
+    public authService: AuthService
   ) { }
 
   ngOnInit(): void {
     this.loadRecipes();
   }
 
+  handleSorting() {
+    return (this.recipeList)
+      ? (this.recipeList?.reverse() && this.sortingType === 'oldest')
+        ? this.sortingType = 'newest'
+        : this.sortingType = 'oldest'
+      : this.authService.hasError = 'There are no receipes to load or the server is not responding!'
+  }
+
   loadRecipes(): void {
     this.globalLoaderService.showLoader('Loading');
     this.recipeService.loadRecipes().subscribe({
       next: (recipeList) => {
-        if (recipeList !== undefined && recipeList !== null) {
+        if (recipeList) {
           this.globalLoaderService.hideLoader();
-          this.recipeList = recipeList;
+          this.recipeList = recipeList.filter(a => !a.isDeleted).reverse();
         }
+      }, error: (err) => {
+        if (!err.ok) {
+          console.error(err.message);
+          this.globalLoaderService.hideLoader(false);
+          return this.authService.hasError = 'There is no connection to the server right now!';
+        }
+        console.error(err.error.message);
+        this.globalLoaderService.hideLoader(false);
+        return this.authService.hasError = err.error.messsage;
       }
     });
   }
