1d1c7c6ba110482c072c050fa4f1e23116d6c564
Martin B <m.bogdanov9110@gmail.com>
2022-12-08 17:16:39 +0200

    update

--- src/app/Guards/auth-guard.ts
+++ src/app/Guards/auth-guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
-import { getSession } from "../API/session";
 
+import { getSession } from "../API/session";
 import { AuthService } from './../auth/auth.service';
 
 
