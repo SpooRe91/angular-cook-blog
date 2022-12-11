b31e1702d4060613866f0b5ea02b7bc6d16a642d
Martin B <m.bogdanov9110@gmail.com>
2022-12-11 21:45:15 +0200

    update on styling logic and comp of details

--- src/app/recipes/details/details.component.ts
+++ src/app/recipes/details/details.component.ts
@@ -1,9 +1,11 @@
+import { Title } from '@angular/platform-browser';
 import { AuthService } from './../../auth/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { RecipeService } from '../recipe.service';
 import { GlobalLoaderService } from '../../shared/services/global-loader.service';
+import { first } from 'rxjs';
 
 
 @Component({
@@ -14,46 +16,49 @@ import { GlobalLoaderService } from '../../shared/services/global-loader.service
 export class RecipeDetails implements OnInit {
 
   alt: string = "#";
-
   params: string | number;
   date: string | Date = '';
 
   constructor(
+    private title: Title,
+    public authService: AuthService,
     public recipeService: RecipeService,
-    public globalLoaderService: GlobalLoaderService,
     private activatedRoute: ActivatedRoute,
-    public authService: AuthService
+    public globalLoaderService: GlobalLoaderService,
   ) {
     this.params = this.activatedRoute.snapshot.params['id'];
   }
 
+  handleRecipeDelete() {
+    return this.recipeService.deleteRecipe(this.params);
+  }
+
   ngOnInit(): void {
     this.globalLoaderService.showLoader("Loading", true);
-    this.recipeService.loadRecipeDetails(this.params).subscribe({
-
-      next: (value) => {
-        if (value === null && value === undefined) { return }
-        this.recipeService.recipeDetails = value;
-        this.date = new Date(value.createdAt.toString());
-        this.date = this.date.toString()
-          .slice(0, this.date
-            .toString()
-            .indexOf('GMT'));
-        console.log(typeof this.date);
-        console.log(this.recipeService.recipeDetails);
-        this.globalLoaderService.hideLoader();
-        return
-      },
-      error: (err) => {
-        if (!err.ok) {
-          console.error(err.message);
+    this.recipeService.loadRecipeDetails(this.params).
+      pipe(first()).subscribe({
+        next: (value) => {
+          if (value === null && value === undefined) { return };
+          this.recipeService.recipeDetails = value;
+          this.date = new Date(value.createdAt.toString());
+          this.date = this.date.toString()
+            .slice(0, this.date
+              .toString()
+              .indexOf('GMT'));
+          this.globalLoaderService.hideLoader();
+          this.title.setTitle(`Details ${this.recipeService.recipeDetails.name}`);
+          return
+        },
+        error: (err) => {
+          if (!err.ok) {
+            console.error(err.message);
+            this.globalLoaderService.hideLoader(false);
+            return this.authService.hasError = 'There is no connection to the server right now!';
+          }
+          console.error(err.error.message);
           this.globalLoaderService.hideLoader(false);
-          return this.authService.hasError = 'There is no connection to the server right now!';
+          return this.authService.hasError = err.error.messsage;
         }
-        console.error(err.error.message);
-        this.globalLoaderService.hideLoader(false);
-        return this.authService.hasError = err.error.messsage;
-      }
-    });
+      });
   };
 };
