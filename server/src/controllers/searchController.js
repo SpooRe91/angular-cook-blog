1ccfaab6febe12e9b506ad00c37ee63ff43f5280
Martin B <m.bogdanov9110@gmail.com>
2022-12-15 23:50:52 +0200

    updates on server

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -5,49 +5,49 @@ const { getErrorMessage } = require('../utils/errorHelpers');
 
 router.get('/browse', async (req, res) => {
 
-    try {
-        let allMeals = await foodService.getAll(req.query.search);
+  try {
+    let allMeals = await foodService.getAll(req.query.search);
 
-        if (allMeals.length <= 0) {
-            throw new Error("Не можахме да намерим рецепти или сървърът не отговаря!");
-        }
-
-        res.status(200).json(allMeals);
-    } catch (error) {
-
-        res.status(400).json({ message: getErrorMessage(error) });
+    if (allMeals.length <= 0) {
+      throw new Error("Не можахме да намерим рецепти или сървърът не отговаря!");
     }
+
+    res.status(200).json(allMeals.filter(x => !x.isDeleted));
+  } catch (error) {
+
+    res.status(400).json({ message: getErrorMessage(error) });
+  }
 
 });
 
 router.get('/myRecipes', isAuth, async (req, res) => {
 
 
-    try {
-        let allMeals = await foodService.getOwn(req.user._id);
+  try {
+    let allMeals = await foodService.getOwn(req.user._id);
 
-        if (!allMeals.length > 0) {
-            throw new Error("Няма намерени рецепти!");
-        }
-        res.status(200).json(allMeals);
-
-    } catch (error) {
-        res.status(400).json({ message: getErrorMessage(error) });
+    if (!allMeals.length > 0) {
+      throw new Error("Няма намерени рецепти!");
     }
+    res.status(200).json(allMeals.filter(x => !x.isDeleted));
+
+  } catch (error) {
+    res.status(400).json({ message: getErrorMessage(error) });
+  }
 });
 
 router.get('/macros', async (req, res) => {
 
-    try {
-        let allMeals = await foodService.getNutritions();
+  try {
+    let allMeals = await foodService.getNutritions();
 
-        if (allMeals.length <= 0) {
-            throw new Error("Ресурсите не могат да бъдат намерени!");
-        };
-        res.status(200).json(allMeals);
-    } catch (error) {
-        res.status(400).json({ message: getErrorMessage(error) });
-    }
+    if (allMeals.length <= 0) {
+      throw new Error("Ресурсите не могат да бъдат намерени!");
+    };
+    res.status(200).json(allMeals);
+  } catch (error) {
+    res.status(400).json({ message: getErrorMessage(error) });
+  }
 
 });
 
--- server/src/index.js
+++ server/src/index.js
@@ -14,9 +14,9 @@ require('./config/expressConfig')(app);//express config
 app.use(errorHandler)//error handler
 
 app.listen(currPort, (error) => {
-    if (error) {
-        console.log(error.message);
-    }
-    console.log(`"Cook-blog API" listening to port http://localhost:${currPort}`);
-    console.log(`"Cook-blog" React APP and REST API documentation is available at: https://github.com/SpooRe91/react-js-project-final/blob/main/Cook-Blog-readme.md`);
+  if (error) {
+    console.log(error.message);
+  }
+  console.log(`"Cook-blog API" listening to port http://localhost:${currPort}`);
+  console.log(`"Cook-blog" Angular APP and REST API documentation is available at: https://github.com/SpooRe91/angular-cook-blog/blob/main/README.md`);
 });
